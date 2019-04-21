2e1721098113d1f763f8a3350f3372a604c8bf30
Saad <saadaliaffandi@gmail.com>
2019-04-22 02:29:28 +0500

    phase 1

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+
+// import the firebase third party lib
+import firebase from "firebase";
+
+// // import header & loginform
+// import {Header} from "./components/common";
+// import LoginForm from "./components//LoginForm";
+
+import { Header } from "./components/common";
+// Import our LoginForm component to be displayed on the screen
+//import LoginForm from "./components/LoginForm";
+
+import LoginForm from "./components/LoginForm";
+
+class App extends Component {
+// Life cycle method to init the firebase
+    componentWillMount() {
+        firebase.initializeApp({
+        apiKey: "AIzaSyBsaXqqEioMPhYrSdSrSd3mCwK0sUHUeAc",
+        authDomain: "auth-e7ac5.firebaseapp.com",
+        databaseURL: "https://auth-e7ac5.firebaseio.com",
+        projectId: "auth-e7ac5",
+        storageBucket: "auth-e7ac5.appspot.com",
+        messagingSenderId: "846221714758"
+        });
+    }
+    render() {
+        return (
+            <View>
+                <Header headerText="Authentication" />
+                <LoginForm />
+            </View>
+        );
+    }   
+}
+
+export default App;
